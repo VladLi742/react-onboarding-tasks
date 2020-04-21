e5567252d824813d601c43c4a1f8882895235dac
Vladislav Likhachev <vladislav.likhachev742@gmail.com>
2020-04-21 13:22:37 +0300

    add src/globalStyles.js
    * font-family: 'Roboto', sans-serif

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    margin: unset;
+    font-family: 'Roboto', sans-serif;
+  }
+  input {
+    font-family: inherit;
+  }
+`;
