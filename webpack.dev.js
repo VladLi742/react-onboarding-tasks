742030690926cd9543d277c9b99241677ca5a333
Vladislav Likhachev <vladislav.likhachev742@gmail.com>
2020-04-22 14:35:16 +0300

    add prettier pre-commit hook and webpack plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,22 @@
 const path = require("path");
 const webpack = require("webpack");
-const merge = require('webpack-merge');
+const merge = require("webpack-merge");
+const PrettierPlugin = require("prettier-webpack-plugin");
 
-const common = require('./webpack.common.js');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: "inline-source-map",
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.jsx', '.js' ],
-        alias: { 'react-dom': '@hot-loader/react-dom' },
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        hot: true,
-        open: true,
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+	mode: "development",
+	devtool: "inline-source-map",
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js"],
+		alias: { "react-dom": "@hot-loader/react-dom" },
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "public/"),
+		port: 3000,
+		hot: true,
+		open: true,
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin(), new PrettierPlugin()],
 });
