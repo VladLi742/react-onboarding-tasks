7f217bc93e62a29a531fa2ab756550d3ffcab349
Vladislav Likhachev <vladislav.likhachev742@gmail.com>
2020-04-24 14:49:03 +0300

    move Filter to ListSubHeader

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { hot } from "react-hot-loader/root";
+import { Container } from "@material-ui/core";
 
 import { store, persistor } from "./redux/store";
 
 import GlobalStyles from "./globalStyles";
-import Filter from "./components/Filter";
 import List from "./components/List";
 
 function App() {
@@ -15,8 +15,9 @@ function App() {
       <PersistGate loading={null} persistor={persistor}>
         <React.StrictMode>
           <GlobalStyles />
-          <Filter />
-          <List />
+          <Container>
+            <List />
+          </Container>
         </React.StrictMode>
       </PersistGate>
     </Provider>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { List as ListMaterial } from "@material-ui/core";
+import { List as ListMaterial, ListSubheader } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+
 import { FETCH_ORDERS } from "../redux/modules/list";
 
 import { AppState, Order } from "../ts/interfaces";
 
 import ListItem from "./ListItem";
+import Filter from "./Filter";
+
+const Header = styled.span`
+  font-size: 20px;
+`;
+
+const useStyles = makeStyles(() => ({
+  listSubHeaderRoot: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
 
 export default function List() {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const { orders, isFetchedOrders } = useSelector(
     (state: AppState) => state.list
@@ -19,7 +35,19 @@ export default function List() {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <ListMaterial component="nav">
+    <ListMaterial
+      component="nav"
+      aria-labelledby="list-subheader"
+      subheader={
+        <ListSubheader
+          component="div"
+          classes={{ root: classes.listSubHeaderRoot }}
+        >
+          <Header>Заказы</Header>
+          <Filter />
+        </ListSubheader>
+      }
+    >
       {isFetchedOrders &&
         orders.map((order: Order) => {
           return <ListItem order={order} key={order.id} />;
