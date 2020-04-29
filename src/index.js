fa521bda3b67bef06e21f7fe52d67d573cbcbd98
Vladislav Likhachev <vladislav.likhachev742@gmail.com>
2020-04-29 10:58:34 +0300

    refactor import regenerator-time in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import regeneratorRuntime from "regenerator-runtime"; // for support async / await
+import "regenerator-runtime"; // for support async / await
 import React from "react";
 import ReactDOM from "react-dom";
 
